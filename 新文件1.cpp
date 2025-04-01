8747525cc5fe4ae37efc2ad3109b418a6676f1be
Oaiusfug <u11302234@o365.ttu.edu.tw>
2025-04-01 19:53:04 +0800

    Add files via upload

---
+++ 新文件1.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+int(viod)
+{
+         printf("I love C language best");
+         printf("/" I love C language best/"");
+         
+         system ("pause");
+         return 0;
